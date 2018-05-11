7db1aa267904dc611b7819aa5e97d2a071a3ce18
kun <kunyuanhuang12@gmail.com>
2018-05-11 15:13:14 -0700

    version 1.0.1

---
+++ PingMachine.ps1
@@ -0,0 +1,46 @@
+﻿<#
+ .Note
+    Author : Rafael,kun,Vincent
+    Version : 1.0.0
+#>
+Clear
+$IP = Read-Host -Prompt 'Ping'
+$Attemp = Read-Host -Prompt 'Times of Attemp'
+<#
+$Output = ''
+$Succeed = 0
+$Failure = 0
+$time = 0  
+#>
+$Output_arr = @()
+While ($Attemp -gt 0){
+   $time += 1
+    if((Test-Connection -computer $IP -Delay 1 -quiet)){
+        $Succeed += 1        
+        $Output = "$time . $(Get-Date -format "dd-MM-yyyy HH:mm:ss"): $IP Successful Connected!"
+        Write-Host -ForegroundColor Green "$Output"
+    }Else {
+        $Failure += 0
+        $Output = "$time . $(Get-Date -format "dd-MM-yyyy HH:mm:ss"): $IP Unable to Connected!"
+        Write-Host -ForegroundColor Red "$Output"
+    }
+     $Output_arr += $Output
+     $Attemp -= 1
+}
+Write-Host "Testing Completed !"
+Write-Host -ForegroundColor Green "Times of Succeed: $Succeed" 
+Write-Host -ForegroundColor Red "Times of Failure: $Failure "
+$Output_arr
+
+$Print = Read-Host -Prompt "Print Reult in Text File (Y/N) "
+if($Print.ToUpper() -eq 'Y'){
+    $FileName = Read-Host -Prompt "Enter File Name "
+    $OutputFile = (Get-Location).Path + "\$FileName.txt"
+    try{
+        $IO = [System.IO.StreamWriter] $OutputFile
+        for ($i=0; $i -lt $Output_arr.Length;$i++){
+            $IO.WriteLine($Output_arr[$i])
+        }
+        $IO.close()
+    }finally{}
+}

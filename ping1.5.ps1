fd9f36de5528909b00d198e16a61c87941ee306d
kun <kunyuanhuang12@gmail.com>
2018-05-15 22:03:55 -0700

    15 may 2018 research

--- ping1.5.ps1
+++ ping1.5.ps1
@@ -5,10 +5,31 @@ $res = Test-Connection 10.0.10.5 -count 6 -ErrorAction Stop | ft @{Name='address
                                         @{Name='ipv4address';Expr={$_.ipv4address};width=12},
                                         @{Name='responsetime';Expr={$_.responsetime};width=15} 
                                         #>
-Test-Connection 10.0.10.5 -count 6 | if(Select-Object -Property Address,ResponseTime,StatusCode 
+Test-Connection 8.8.8.8 -count 6 | Select-Object -Property Address,ResponseTime,StatusCode
 #Test-Connection 8.8.8.8 -count 6 | % {Write-Host "Reply from $($_.IPV4Address): bytes=$($_.BufferSize) time=$($_.ResponseTime) TTL=$($_.ResponseTimeToLive)"}
 } catch [System.Net.NetworkInformation.PingException]  {
   write-output 'request timed out'
 }
+
+<#
+clear
+<#
+$Error.Clear()
+Test-Connection 10.0.10.5, 8.8.8.8 -ea 0 | %{if($Error.Count -gt 0){
+        Write-Host "Request Time Out" 
+        $Error.Clear()
+        $Error.Count
+    }Else{
+        "Reply from $($_.IPV4Address): bytes=$($_.BufferSize) time=$($_.ResponseTime) TTL=$($_.ResponseTimeToLive)"
+   }
+}
+#>
+<#
+#Test-Connection 8.8.8.8,10.0.10.5 -ea 2 | %{$Error.Count}
+$Output_arr = @()
+#Test-Connection 10.0.10.5 -Count 4| %{$_;$Output_arr += $_} Get-Member
+Test-Connection 8.8.8.8 -Count 1 |  Get-Member
+$Output_arr
+#>
 
 
